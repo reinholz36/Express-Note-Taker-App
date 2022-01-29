cd010e580f9f83aae4eaf9989473d2101253f4c1
reinholz36 <dreinholz28@gmail.com>
2022-01-29 16:36:18 -0500

    add heroku

---
+++ server.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+// const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+
+const PORT = process.env.PORT || 3001
+
+const app = express();
+
+app.use(express.static('public'));
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
+
+// app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+ console.log(`API server on port ${PORT}!`);
+});
